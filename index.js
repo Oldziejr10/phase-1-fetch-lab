5be4103698a27a32ffd9b8b981d68037ce149e1d
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-06-20 22:51:12 -0400

    commit tree d4f4a90c4e8fab89436111f1912a3abaaa0a4d36

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(books => renderBooks(books))
 }
 
-function renderBooks(books) {
+function renderBooks(book) {
   const main = document.querySelector('main');
-  books.forEach(book => {
+  book.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  return main;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
